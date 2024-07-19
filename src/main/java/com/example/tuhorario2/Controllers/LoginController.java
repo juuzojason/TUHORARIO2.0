dad67857c2ccf67dd214ade69f4da83da350f2b1
juuzojason <juan.tobijose@gmail.com>
2024-07-19 15:20:27 -0500

    Controller
    D

--- src/main/java/com/example/tuhorario2/Controllers/LoginController.java
+++ src/main/java/com/example/tuhorario2/Controllers/LoginController.java
@@ -1,5 +1,17 @@
 package com.example.tuhorario2.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
 
+    public TextField txtuser;
+    public PasswordField txtpass;
+    public Button loginbtn;
+    public Hyperlink singup;
+    public Button Exitbtn;
+    public Button movebtn;
+    public Button minbtn;
 }
