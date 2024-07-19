b1e257c7cbe2d361f356c6e63a0af74b09a528e6
charly1616 <124099579+charly1616@users.noreply.github.com>
2024-07-19 14:19:58 -0500

    .

--- src/main/java/com/example/tuhorario2/Models/DBDriver.java
+++ src/main/java/com/example/tuhorario2/Models/DBDriver.java
@@ -4,4 +4,36 @@ import java.sql.Connection;
 
 public class DBDriver {
     Connection c;
+
+
+    //TODO function that logins to a USER given its username and password
+
+    //TODO function that creates a new group
+
+    //TODO function that creates a new course
+
+    //TODO fuction that creates a new option
+    // - needs to add all labels to the table as well
+    // - also needs to add all the days to the respective table
+
+    //Note MUST CHECK IF THE USER IS THE OWNER
+    //TODO function that updates the group
+    //TODO function that deletes group
+
+    //Note MUST CHECK IF THE USER IS THE OWNER
+    //TODO function that updates course
+    //TODO function that delete course
+
+    //Note MUST CHECK IF THE USER IS THE OWNER
+    //TODO function that updates the option
+    // - must update all labels as well
+    // - must update all the days as well
+
+    //TODO function that delete the option
+    // - must delete all the label as well
+    // - must delete all days as well
+
+    //TODO select functions
+
+
 }
