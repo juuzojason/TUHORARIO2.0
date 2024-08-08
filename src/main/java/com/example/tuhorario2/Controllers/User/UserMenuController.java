32663d79e5e109414951d61f070f093f0719d6de
charly1616 <124099579+charly1616@users.noreply.github.com>
2024-08-07 22:08:01 -0500

    fxml

--- src/main/java/com/example/tuhorario2/Controllers/User/UserMenuController.java
+++ src/main/java/com/example/tuhorario2/Controllers/User/UserMenuController.java
@@ -2,6 +2,7 @@ package com.example.tuhorario2.Controllers.User;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -9,8 +10,11 @@ import java.util.ResourceBundle;
 
 public class UserMenuController extends VBox implements Initializable {
     public Button GenerateButton;
-    public Button FiltersButton;
     public Button EditButton;
+    public Button LogOutBtn;
+    public Button WatchButton;
+    public Label UsernameLabel;
+    public Label GroupNumberLabel;
     public Button ThemesButton;
 
     @Override
